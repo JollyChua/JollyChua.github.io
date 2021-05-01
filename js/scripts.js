fccbaf469de081470b7bab263c48b64a7d7529ee
JollyChua <74226122+JollyChua@users.noreply.github.com>
2021-05-01 21:27:19 +0800

    updated file

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
+    prompt("Who are you?");
+
     (function ($) {
     "use strict"; // Start of use strict
 
